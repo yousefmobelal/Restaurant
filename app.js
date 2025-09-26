f646fb87a786c4c70f3450de181e83775a3dcab9
yousefmobelal <ym8370302@gmail.com>
2025-09-26 22:56:53 +0300

    - add css url for swagger

--- app.js
+++ app.js
@@ -14,7 +14,18 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    customCss:
+      ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
+    customCssUrl: CSS_URL,
+  })
+);
 
 app.get("/swagger.json", (req, res) => {
   res.setHeader("Content-Type", "application/json");
