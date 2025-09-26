1a6897dea83c3312ea7cd3fb07c54cbbfc4d343b
yousefmobelal <ym8370302@gmail.com>
2025-09-26 23:14:35 +0300

    add path to swagger docs files

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -1,5 +1,6 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import path from "path";
 
 const url =
   process.env.BASE_URL || `http://localhost:${process.env.PORT || 3000}/api/`;
@@ -24,7 +25,9 @@ const options = {
       },
     },
   },
-  apis: ["./src/routes/*.js", "./src/controllers/*.js", "./docs/*.js"],
+  apis: ["src/routes/*.js", "src/controllers/*.js", "docs/*.js"].map(
+    (filePath) => path.join(process.cwd(), filePath)
+  ),
 };
 
 const swaggerSpec = swaggerJsdoc(options);
