3ab084fce7fab607f8fed77009db853eca0ecbc6
yousefmobelal <ym8370302@gmail.com>
2025-09-26 22:14:16 +0300

    -fix bugs of swagger doesn't work with vercel

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    swaggerOptions: { url: "/swagger.json" },
+  })
+);
 app.get("/swagger.json", (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.send(swaggerSpec);
